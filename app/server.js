4cd1cd2e804a5659f771e2bccb11c16127d84544
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2021-09-23 15:52:49 +1000

    updated server

--- app/server.js
+++ app/server.js
@@ -1,4 +1,21 @@
 const { createServer } = require("http");
+const next = require("next");
+
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on <http://localhost>:${port}`);
+  });
+});
+
+/* const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
@@ -27,3 +44,4 @@ app.prepare().then(() => {
     console.log("> Ready on http://localhost:", port);
   });
 });
+ */
