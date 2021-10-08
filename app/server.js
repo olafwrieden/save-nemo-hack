8a7e17ac0c71b6b7ed6daf8b6e7615a1368f407e
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2021-10-08 20:16:57 +1000

    updated server to koa

--- app/server.js
+++ app/server.js
@@ -1,16 +1,28 @@
-const { createServer } = require("http-server");
+const Koa = require("koa");
 const next = require("next");
+const Router = require("@koa/router");
 
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on <http://localhost>:${port}`);
+  const server = new Koa();
+  const router = new Router();
+
+  router.all("(.*)", async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+  });
+
+  server.use(router.routes());
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
