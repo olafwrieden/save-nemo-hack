dbe619a66438cda6e8f1d11eb46ce64789d08cd0
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2021-10-11 16:13:28 +1000

    changed next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -6,7 +6,6 @@ module.exports = {
     return config;
   },
   env: {
-    NODE_ENV: process.env.NODE_ENV || "development",
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
     NEXT_PUBLIC_AZURE_AD_CLIENT_ID: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID,
     NEXT_PUBLIC_AZURE_AD_TENANT_ID: process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID,
