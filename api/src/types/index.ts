e4e6ca94a4822aee613e582a87a8b93323b8ee09
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2021-11-17 23:58:56 +1000

    Added Smart Buoy Interfaces

--- api/src/types/index.ts
+++ api/src/types/index.ts
@@ -12,9 +12,37 @@ export interface IOrganization {
   members: IMember[];
 }
 
-export interface IBouy{
+export interface IBouy {
   id: Number;
   name: string;
   owner_org: string;
+}
 
+export interface ISmartBuoyConfig {
+  maxDriftDistance: number; // 0 = unknown
+  osVersion: string;
+  uptime: number;
+  powerUpTime: string; // something like '05:30'
+  powerDownTime: string; // something like '22:45'
+}
+
+export interface ISmartBuoyVitals {
+  latitude: string;
+  longitude: string;
+  gsmStrength: string;
+  batteryLevel: number;
+  ipAddress: string;
+  solarVoltage: string;
+  solarAmperage: string;
+  lastActivityTime: string;
+}
+export interface ISmartBuoy {
+  id: string;
+  deviceId: string;
+  ownerId: string;
+  isDeployed: boolean;
+  isPublic: boolean;
+  isEnabled: boolean;
+  config: ISmartBuoyConfig;
+  vitals: ISmartBuoyVitals;
 }
